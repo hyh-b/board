d86fec8cda4e929364e5c147667faebdf101b7de
hyh-b <kash4763@naver.com>
2023-11-16 19:03:17 +0900

    배포자동화

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,6 +2,7 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing //JPA Auditing 활성화
@@ -10,7 +11,9 @@ public class BoardApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(BoardApplication.class, args);
+		SpringApplication application = new SpringApplication(BoardApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
